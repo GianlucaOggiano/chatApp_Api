0aa9cff2cde156e7d85e4deacc47c571297cb81f
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-27 15:46:32 +0100

    authentication routes

---
+++ src/routes/auth/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { AuthController } from '../../controllers';
+
+const router = Router();
+
+router.post('/signup', AuthController.signUp);
+router.post('/signin', AuthController.signIn);
+
+export default router;
