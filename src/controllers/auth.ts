5462527d8c79661a5764fa615b99e5561c937a64
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-27 16:11:29 +0100

    add signup validation

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,25 +2,30 @@ import { NextFunction, Request, Response } from 'express';
 
 import { HttpException } from '../error';
 import { User } from '../models';
+import { signupValidator, validate } from '../validators';
 
 export const signUp = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const { email, password } = req.body;
+  const { displayName, email, password } = req.body;
   try {
+    await validate(signupValidator, { displayName, email, password });
+
     const userAlreadyExist = await User.exists({ email });
     if (userAlreadyExist) {
       throw new HttpException(422, 'Invalid email or passsword.Please retry.');
     }
 
     const newUser = new User({
+      displayName,
       email,
       password,
     });
     await newUser.save();
     req.session.userId = newUser.id;
+
     res.json(newUser);
   } catch (error) {
     next(error);
