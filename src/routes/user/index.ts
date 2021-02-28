7b603f20e40e220fe3b1251a3b3e9f7a00a53f29
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-28 11:31:51 +0100

    add get profile and update routes

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import { UserController } from '../../controllers';
+import { isAuth } from '../../middleware/auth';
 
 const router = Router();
+
+router.get('/:id', isAuth, UserController.profile);
+router.put('/:id', isAuth, UserController.updateProfile);
 
 export default router;
