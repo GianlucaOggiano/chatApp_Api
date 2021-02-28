79748bbf844a0c6d11880aba3d77f17737b2eefb
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-28 11:31:27 +0100

    add get profile and update

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,1 +1,33 @@
-export {};
+import { NextFunction, Request, Response } from 'express';
+import { User } from '../models';
+import { updateProfileValidator, validate } from '../validators';
+
+export const profile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  try {
+    await validate(updateProfileValidator, req.body);
+    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
